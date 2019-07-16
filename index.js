50e39f3641529619accc0dd504c1e2c874ae5d43
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-16 12:43:49 +0200

    Tasks styling and final js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const taskContainerEl = document.querySelector('.task-container');
 const addingButtonEl = document.querySelector('.add__button');
 const taskInputEl = document.querySelector('.add-task-modal__content');
 const addTaskModalEl = document.querySelector('.add-task-modal');
+const addTaskContentEl = document.querySelector('.add-task-modal__content');
 const addTaskButtonEl = document.querySelector('.add-task-modal__button');
 const transparentEl = document.querySelector('.transparent-layer');
 const taskCheckboxes = [];
@@ -36,15 +37,25 @@ function toggleClass(element, className) {
 }
 
 function createTask(text) {
+    taskCounter += 1;
+
     const taskItemEl = document.createElement('li');
+    taskItemEl.classList.add('task-item');
 
     const checkboxEl = document.createElement('input');
     checkboxEl.type = 'checkbox';
-    checkboxEl.id = `task-${taskCounter + 1}`;
-    taskCheckboxes.push(checkboxEl);
+    checkboxEl.id = `task-${taskCounter}`;
+
+    const labelEl = document.createElement('label');
+    labelEl.classList.add('task__label');
+    labelEl.setAttribute('for', `task-${taskCounter}`);
+    taskItemEl.appendChild(labelEl);
+    taskCheckboxes.push(labelEl);
+
 
     const taskTextEl = document.createElement('h2');
-    taskTextEl.setAttribute('data-id', `task-${taskCounter + 1}`);
+    taskTextEl.classList.add('task__title');
+    taskTextEl.setAttribute('data-id', `task-${taskCounter}`);
     const taskText = document.createTextNode(text);
     taskTextEl.appendChild(taskText);
 
@@ -64,6 +75,7 @@ function createTask(text) {
 function handlerAddButton() {
     toggleClass(addTaskModalEl, '--active');
     toggleClass(transparentEl, '--active');
+    addTaskContentEl.focus();
 }
 
 addingButtonEl.addEventListener('click', handlerAddButton);
@@ -77,7 +89,8 @@ function handleAddTaskButton() {
 addTaskButtonEl.addEventListener('click', handleAddTaskButton);
 
 function handleCheckbox(event) {
-    const taskIdentifier = event.currentTarget.id;
+    const taskIdentifier = event.currentTarget.getAttribute('for');
     document.getElementById(taskIdentifier).checked ? false : true;
     toggleClass(document.querySelector(`h2[data-id=${taskIdentifier}]`), '--crossed');
+    toggleClass(document.querySelector(`label[for=${taskIdentifier}]`), '--active');
 }
