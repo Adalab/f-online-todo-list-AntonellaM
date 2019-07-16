5085e9501132c0cb44068a66de2e95f0d589c434
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-16 14:51:07 +0200

    Add and recover LocalStorage

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('hello')
-
 const dateContainerEl = document.querySelector('.card-header__date');
 const taskContainerEl = document.querySelector('.task-container');
 const addingButtonEl = document.querySelector('.add__button');
@@ -12,14 +10,32 @@ const addTaskButtonEl = document.querySelector('.add-task-modal__button');
 const transparentEl = document.querySelector('.transparent-layer');
 const taskCheckboxes = [];
 let taskCounter = 0;
+let taskList = [];
 setDate();
+getLocalStorage('tasks', taskList);
+// const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+// console.log(savedTasks);
 
-function createElement(element, className, text, parent) {
+function setLocalStorage(name, object) {
+    localStorage.setItem(name, JSON.stringify(object));
+}
+
+function getLocalStorage(name, destination) {
+    const savedTasks = JSON.parse(localStorage.getItem(name));
+    destination = savedTasks;
+    console.log(savedTasks);
+    savedTasks && destination.forEach(element => createTask(element.task, element.checked));
+    toggleClass(addTaskModalEl, '--active');
+    toggleClass(transparentEl, '--active');
+}
+
+function createElement(element, classNames, text, parent) {
     const newElement = document.createElement(element);
-    newElement.classList.add(className);
+    classNames && classNames.forEach(className => newElement.classList.add(className));
     const newText = document.createTextNode(text);
-    newElement.appendChild(newText);
-    parent.appendChild(newElement);
+    text && newElement.appendChild(newText);
+    parent && parent.appendChild(newElement);
+    return newElement;
 }
 
 function setDate() {
@@ -27,38 +43,31 @@ function setDate() {
     const currentDate = new Date().toLocaleDateString('es-ES', options).replace(new RegExp('de ', 'g'), '').replace(',', '');
     const dateArray = currentDate.split(' ');
 
-    createElement('p', 'weekday', dateArray[0], dateContainerEl);
-    createElement('p', 'day', dateArray[1], dateContainerEl);
-    createElement('p', 'month-year', (dateArray[2] + ', ' + dateArray[3]), dateContainerEl);
+    createElement('p', ['weekday'], dateArray[0], dateContainerEl);
+    createElement('p', ['day'], dateArray[1], dateContainerEl);
+    createElement('p', ['month-year'], (dateArray[2] + ', ' + dateArray[3]), dateContainerEl);
 }
 
 function toggleClass(element, className) {
     element.classList.toggle(className);
 }
 
-function createTask(text) {
+function createTask(text, state) {
     taskCounter += 1;
 
-    const taskItemEl = document.createElement('li');
-    taskItemEl.classList.add('task-item');
-
-    const checkboxEl = document.createElement('input');
+    const taskItemEl = createElement('li', ['task-item']);
+    const checkboxEl = createElement('input')
     checkboxEl.type = 'checkbox';
     checkboxEl.id = `task-${taskCounter}`;
+    state ? checkboxEl.checked === true : checkboxEl.checked === false;
 
-    const labelEl = document.createElement('label');
-    labelEl.classList.add('task__label');
+    const labelEl = createElement('label', ['task__label'], null, taskItemEl);
     labelEl.setAttribute('for', `task-${taskCounter}`);
-    taskItemEl.appendChild(labelEl);
     taskCheckboxes.push(labelEl);
 
-
-    const taskTextEl = document.createElement('h2');
-    taskTextEl.classList.add('task__title');
+    const taskTextEl = createElement('h2', ['task__title'], text); 
     taskTextEl.setAttribute('data-id', `task-${taskCounter}`);
-    const taskText = document.createTextNode(text);
-    taskTextEl.appendChild(taskText);
-
+    
     taskItemEl.appendChild(checkboxEl);
     taskItemEl.appendChild(taskTextEl);
     taskContainerEl.appendChild(taskItemEl);
@@ -67,9 +76,12 @@ function createTask(text) {
     toggleClass(transparentEl, '--active');
 
     taskCheckboxes.forEach(checkbox => {
-        console.log(checkbox);
         checkbox.addEventListener('click', handleCheckbox)
     });
+
+    const taskObject = {'task': taskTextEl.innerHTML, 'checked': checkboxEl.checked};
+
+    return taskObject;
 }
 
 function handlerAddButton() {
@@ -82,8 +94,10 @@ addingButtonEl.addEventListener('click', handlerAddButton);
 
 function handleAddTaskButton() {
     const newTask = taskInputEl.value;
-    createTask(newTask);
     taskInputEl.value = '';
+    const taskObject = createTask(newTask);
+    taskList.push(taskObject);
+    setLocalStorage('tasks', taskList);
 }
 
 addTaskButtonEl.addEventListener('click', handleAddTaskButton);
@@ -91,6 +105,7 @@ addTaskButtonEl.addEventListener('click', handleAddTaskButton);
 function handleCheckbox(event) {
     const taskIdentifier = event.currentTarget.getAttribute('for');
     document.getElementById(taskIdentifier).checked ? false : true;
+    document.querySelector(`h2[data-id=&{taskIdentifier}]`);
     toggleClass(document.querySelector(`h2[data-id=${taskIdentifier}]`), '--crossed');
     toggleClass(document.querySelector(`label[for=${taskIdentifier}]`), '--active');
 }
